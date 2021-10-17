60eb2322084c9baf86b7898ed10183012f44f9e5
Scubasausagee <alekalek2000@gmail.com>
2021-10-17 17:49:46 +0200

    Setting up express and 2 API endpoints

---
+++ src/models/user.js
@@ -0,0 +1,43 @@
+const mongoose=require('mongoose')
+const validator=require('validator')
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 7,
+        validate(value) {
+            if (validator.contains(value.toLowerCase(), "password")) {
+                throw new Error("Can not contain word password in your password")
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a pozitive number')
+            }
+        }
+    }
+})
+
+module.exports= User
