753c85c59f153535ba674c49649064a71c86a7c0
Scubasausagee <alekalek2000@gmail.com>
2021-10-19 20:07:06 +0200

    Creating 4 more API routes and setting up express routers

--- src/index.js
+++ src/index.js
@@ -1,88 +1,15 @@
 const express=require('express')
 require('./DB/mongose')
-const User=require('./models/user')
-const Task=require('./models/task')
+const UserRouter=require('./Routers/user')
+const TaskRouter=require('./Routers/task')
 
 const app=express()
 const port=process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post('/users', async(req,res)=>{
-    const user=new User(req.body)
-
-    try{
-        await user.save()
-        res.status(201).send(user)
-    }catch(e){
-        res.status(400).send(e)
-    }
-})
-
-app.post('/tasks',async(req,res)=>{
-    const task=new Task(req.body)
-
-    try{
-        await task.save()
-        res.status(201).send(task)
-    }catch(e){
-        res.status(400).send(e)
-    }
-})
-
-app.get('/users',async(req,res)=>{
-
-    try{
-        const user=await User.find()
-        res.send(user)
-    }catch(e){
-        res.status(500).send()
-    }
-})
-
-app.get('/users/:id',async(req,res)=>{
-    const _id=req.params.id
-
-    try{
-        const user=await User.findById(_id)
-        if (!user) {
-            return res.status(404).send("User not found")
-        }
-        res.send(user)
-    }catch(e){
-        if (e.kind === "ObjectId") {
-            res.status(400).send("Invalid user id format")
-        }
-        res.status(500).send()
-    }
-})
-
-app.get('/tasks',async(req,res)=>{
-
-    try{
-        const tasks=await Task.find()
-        res.send(tasks)
-    }catch(e){
-        res.status(500).send()
-    }
-})
-
-app.get('/tasks/:id',async(req,res)=>{
-    const _id=req.params.id
-
-    try{
-        const task=await Task.findById(_id)
-        if (!task) {
-            res.status(404).send('User not found')
-        }
-        res.send(task)
-    }catch(e){
-        if (e.kind === "ObjectId") {
-            res.status(400).send("Invalid user id format")
-        }
-        res.status(500).send()
-    }
-})
+app.use(UserRouter)
+app.use(TaskRouter)
 
 app.listen(port,()=>{
     console.log('Server up on port '+port)
