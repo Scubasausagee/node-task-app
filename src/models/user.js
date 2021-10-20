67dc4f1ce04a482b1eb993a83dc83589f73c8bd8
Scubasausagee <alekalek2000@gmail.com>
2021-10-20 19:32:52 +0200

    Implementing token generation for each user with JSONWebToken

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
 const validator=require('validator')
 const bcrypt=require('bcryptjs')
-
+const jwt=require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
     name: {
@@ -11,6 +11,7 @@ const userSchema = mongoose.Schema({
     },
     email: {
         type: String,
+        unique: true,
         required: true,
         trim: true,
         lowercase: true,
@@ -39,9 +40,16 @@ const userSchema = mongoose.Schema({
                 throw new Error('Age must be a pozitive number')
             }
         }
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required:true
+        }
+    }]
 })
 
+//Hash the plain text password before saving
 userSchema.pre('save',async function(next){
     const user=this
 
@@ -51,6 +59,33 @@ userSchema.pre('save',async function(next){
 
     next()
 })
+
+userSchema.methods.generateAuthToken = async function(){
+    const user=this
+    const token=jwt.sign({_id: user._id.toString()},'thisismysignvalue')
+
+    user.tokens= user.tokens.concat({token})
+    await user.save()
+
+    return token
+}
+
+//Method used for user login in
+userSchema.statics.findByCredentials= async (email,password)=>{
+    const user= await User.findOne({email})
+
+    if(!user){
+        throw new Error('Unable to log in')
+    }
+
+    const isMatch= await bcrypt.compare(password,user.password)
+
+    if(!isMatch){
+        throw new Error('Unable to log in')
+    }
+
+    return user
+}
 
 const User = mongoose.model('User', userSchema)
 
