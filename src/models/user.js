2b9b87a24f477c87601ab19f30c16f5d3b8d0bbf
Scubasausagee <alekalek2000@gmail.com>
2021-10-22 13:53:06 +0200

    Added support for file uploading and deleting and added data normalization for files

--- src/models/user.js
+++ src/models/user.js
@@ -42,6 +42,9 @@ const userSchema = new mongoose.Schema({
             }
         }
     },
+    avatar:{
+        type: Buffer
+    },
     tokens: [{
         token: {
             type: String,
@@ -76,6 +79,7 @@ userSchema.methods.toJSON = function(){
 
     delete userObject.password
     delete userObject.tokens
+    delete userObject.avatar
 
     return userObject
 }
